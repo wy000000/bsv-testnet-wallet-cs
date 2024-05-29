9972a5d349e362bd448834c599f5208462132147
wy000000 <vw0000@126.com>
2024-05-29 13:28:00 +0800

    字体5号

--- bsv testnet wallet/form bsv testnet wallet.Designer.cs
+++ bsv testnet wallet/form bsv testnet wallet.Designer.cs
@@ -29,6 +29,7 @@
 		private void InitializeComponent()
 		{
 			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.textBox3 = new System.Windows.Forms.TextBox();
 			this.textBox8 = new System.Windows.Forms.TextBox();
 			this.label9 = new System.Windows.Forms.Label();
 			this.label8 = new System.Windows.Forms.Label();
@@ -41,7 +42,6 @@
 			this.textBox6 = new System.Windows.Forms.TextBox();
 			this.textBox5 = new System.Windows.Forms.TextBox();
 			this.textBox4 = new System.Windows.Forms.TextBox();
-			this.textBox3 = new System.Windows.Forms.TextBox();
 			this.textBox2 = new System.Windows.Forms.TextBox();
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.label6 = new System.Windows.Forms.Label();
@@ -93,24 +93,31 @@
 			// splitContainer1.Panel2
 			// 
 			this.splitContainer1.Panel2.Controls.Add(this.flowLayoutPanel1);
-			this.splitContainer1.Size = new System.Drawing.Size(943, 806);
-			this.splitContainer1.SplitterDistance = 350;
+			this.splitContainer1.Size = new System.Drawing.Size(849, 725);
+			this.splitContainer1.SplitterDistance = 314;
 			this.splitContainer1.TabIndex = 0;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(120, 104);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(720, 28);
+			this.textBox3.TabIndex = 23;
 			// 
 			// textBox8
 			// 
-			this.textBox8.Location = new System.Drawing.Point(615, 259);
+			this.textBox8.Location = new System.Drawing.Point(554, 233);
 			this.textBox8.Name = "textBox8";
 			this.textBox8.ReadOnly = true;
-			this.textBox8.Size = new System.Drawing.Size(318, 35);
+			this.textBox8.Size = new System.Drawing.Size(287, 28);
 			this.textBox8.TabIndex = 28;
 			// 
 			// label9
 			// 
 			this.label9.AutoSize = true;
-			this.label9.Location = new System.Drawing.Point(499, 270);
+			this.label9.Location = new System.Drawing.Point(449, 243);
 			this.label9.Name = "label9";
-			this.label9.Size = new System.Drawing.Size(130, 24);
+			this.label9.Size = new System.Drawing.Size(98, 18);
 			this.label9.TabIndex = 35;
 			this.label9.Text = "网络状态：";
 			// 
@@ -118,44 +125,44 @@
 			// 
 			this.label8.AutoSize = true;
 			this.label8.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-			this.label8.Location = new System.Drawing.Point(15, 12);
+			this.label8.Location = new System.Drawing.Point(14, 11);
 			this.label8.Name = "label8";
-			this.label8.Size = new System.Drawing.Size(385, 24);
+			this.label8.Size = new System.Drawing.Size(324, 20);
 			this.label8.TabIndex = 34;
 			this.label8.Text = "输入学号登录会自动生成固定密钥";
 			// 
 			// button2
 			// 
-			this.button2.Location = new System.Drawing.Point(615, 43);
+			this.button2.Location = new System.Drawing.Point(554, 39);
 			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(170, 35);
+			this.button2.Size = new System.Drawing.Size(153, 32);
 			this.button2.TabIndex = 33;
 			this.button2.Text = "加解密与签名验证";
 			this.button2.UseVisualStyleBackColor = true;
 			// 
 			// button4
 			// 
-			this.button4.Location = new System.Drawing.Point(210, 309);
+			this.button4.Location = new System.Drawing.Point(189, 278);
 			this.button4.Name = "button4";
-			this.button4.Size = new System.Drawing.Size(73, 35);
+			this.button4.Size = new System.Drawing.Size(66, 32);
 			this.button4.TabIndex = 32;
 			this.button4.Text = "币";
 			this.button4.UseVisualStyleBackColor = true;
 			// 
 			// button3
 			// 
-			this.button3.Location = new System.Drawing.Point(131, 309);
+			this.button3.Location = new System.Drawing.Point(118, 278);
 			this.button3.Name = "button3";
-			this.button3.Size = new System.Drawing.Size(73, 35);
+			this.button3.Size = new System.Drawing.Size(66, 32);
 			this.button3.TabIndex = 31;
 			this.button3.Text = "交易";
 			this.button3.UseVisualStyleBackColor = true;
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(510, 43);
+			this.button1.Location = new System.Drawing.Point(459, 39);
 			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(73, 35);
+			this.button1.Size = new System.Drawing.Size(66, 32);
 			this.button1.TabIndex = 30;
 			this.button1.Text = "登录";
 			this.button1.UseVisualStyleBackColor = true;
@@ -163,124 +170,117 @@
 			// label7
 			// 
 			this.label7.AutoSize = true;
-			this.label7.Location = new System.Drawing.Point(12, 125);
+			this.label7.Location = new System.Drawing.Point(11, 112);
 			this.label7.Name = "label7";
-			this.label7.Size = new System.Drawing.Size(130, 24);
+			this.label7.Size = new System.Drawing.Size(98, 18);
 			this.label7.TabIndex = 29;
 			this.label7.Text = "钱包密钥：";
 			// 
 			// textBox7
 			// 
-			this.textBox7.Location = new System.Drawing.Point(133, 259);
+			this.textBox7.Location = new System.Drawing.Point(120, 233);
 			this.textBox7.Name = "textBox7";
 			this.textBox7.ReadOnly = true;
-			this.textBox7.Size = new System.Drawing.Size(347, 35);
+			this.textBox7.Size = new System.Drawing.Size(313, 28);
 			this.textBox7.TabIndex = 26;
 			// 
 			// textBox6
 			// 
-			this.textBox6.Location = new System.Drawing.Point(133, 223);
+			this.textBox6.Location = new System.Drawing.Point(120, 201);
 			this.textBox6.Name = "textBox6";
-			this.textBox6.Size = new System.Drawing.Size(800, 35);
+			this.textBox6.Size = new System.Drawing.Size(720, 28);
 			this.textBox6.TabIndex = 27;
 			// 
 			// textBox5
 			// 
-			this.textBox5.Location = new System.Drawing.Point(133, 187);
+			this.textBox5.Location = new System.Drawing.Point(120, 168);
 			this.textBox5.Name = "textBox5";
-			this.textBox5.Size = new System.Drawing.Size(800, 35);
+			this.textBox5.Size = new System.Drawing.Size(720, 28);
 			this.textBox5.TabIndex = 25;
 			// 
 			// textBox4
 			// 
-			this.textBox4.Location = new System.Drawing.Point(133, 151);
+			this.textBox4.Location = new System.Drawing.Point(120, 136);
 			this.textBox4.Name = "textBox4";
-			this.textBox4.Size = new System.Drawing.Size(800, 35);
+			this.textBox4.Size = new System.Drawing.Size(720, 28);
 			this.textBox4.TabIndex = 24;
-			// 
-			// textBox3
-			// 
-			this.textBox3.Location = new System.Drawing.Point(133, 115);
-			this.textBox3.Name = "textBox3";
-			this.textBox3.Size = new System.Drawing.Size(800, 35);
-			this.textBox3.TabIndex = 23;
 			// 
 			// textBox2
 			// 
-			this.textBox2.Location = new System.Drawing.Point(133, 79);
+			this.textBox2.Location = new System.Drawing.Point(120, 71);
 			this.textBox2.Name = "textBox2";
-			this.textBox2.Size = new System.Drawing.Size(800, 35);
+			this.textBox2.Size = new System.Drawing.Size(720, 28);
 			this.textBox2.TabIndex = 22;
 			// 
 			// textBox1
 			// 
-			this.textBox1.Location = new System.Drawing.Point(133, 43);
+			this.textBox1.Location = new System.Drawing.Point(120, 39);
 			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(347, 35);
+			this.textBox1.Size = new System.Drawing.Size(313, 28);
 			this.textBox1.TabIndex = 21;
 			// 
 			// label6
 			// 
 			this.label6.AutoSize = true;
-			this.label6.Location = new System.Drawing.Point(12, 269);
+			this.label6.Location = new System.Drawing.Point(11, 242);
 			this.label6.Name = "label6";
-			this.label6.Size = new System.Drawing.Size(82, 24);
+			this.label6.Size = new System.Drawing.Size(62, 18);
 			this.label6.TabIndex = 20;
 			this.label6.Text = "余额：";
 			// 
 			// label5
 			// 
 			this.label5.AutoSize = true;
-			this.label5.Location = new System.Drawing.Point(12, 233);
+			this.label5.Location = new System.Drawing.Point(11, 210);
 			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(82, 24);
+			this.label5.Size = new System.Drawing.Size(62, 18);
 			this.label5.TabIndex = 19;
 			this.label5.Text = "地址：";
 			// 
 			// label4
 			// 
 			this.label4.AutoSize = true;
-			this.label4.Location = new System.Drawing.Point(12, 197);
+			this.label4.Location = new System.Drawing.Point(11, 177);
 			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(130, 24);
+			this.label4.Size = new System.Drawing.Size(98, 18);
 			this.label4.TabIndex = 18;
 			this.label4.Text = "公钥HASH：";
 			// 
 			// label3
 			// 
 			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(12, 161);
+			this.label3.Location = new System.Drawing.Point(11, 145);
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(130, 24);
+			this.label3.Size = new System.Drawing.Size(98, 18);
 			this.label3.TabIndex = 17;
 			this.label3.Text = "压缩公钥：";
 			// 
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(12, 89);
+			this.label2.Location = new System.Drawing.Point(11, 80);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(130, 24);
+			this.label2.Size = new System.Drawing.Size(98, 18);
 			this.label2.TabIndex = 16;
 			this.label2.Text = "原始私钥：";
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(12, 53);
+			this.label1.Location = new System.Drawing.Point(11, 48);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(82, 24);
+			this.label1.Size = new System.Drawing.Size(62, 18);
 			this.label1.TabIndex = 15;
 			this.label1.Text = "学号：";
 			// 
 			// linkLabel1
 			// 
 			this.linkLabel1.AutoSize = true;
-			this.linkLabel1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-			this.linkLabel1.Location = new System.Drawing.Point(803, 47);
+			this.linkLabel1.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.linkLabel1.Location = new System.Drawing.Point(723, 42);
 			this.linkLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.linkLabel1.Name = "linkLabel1";
-			this.linkLabel1.Size = new System.Drawing.Size(130, 24);
+			this.linkLabel1.Size = new System.Drawing.Size(98, 18);
 			this.linkLabel1.TabIndex = 14;
 			this.linkLabel1.TabStop = true;
 			this.linkLabel1.Text = "申请测试币";
@@ -293,16 +293,16 @@
 			this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
 			this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
 			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-			this.flowLayoutPanel1.Size = new System.Drawing.Size(943, 452);
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(849, 407);
 			this.flowLayoutPanel1.TabIndex = 0;
 			// 
 			// Form1
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(943, 806);
+			this.ClientSize = new System.Drawing.Size(849, 725);
 			this.Controls.Add(this.splitContainer1);
-			this.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
 			this.Margin = new System.Windows.Forms.Padding(4);
 			this.Name = "Form1";
 			this.Text = "BSV Testnet Wallet";
